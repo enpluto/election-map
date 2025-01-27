56b936126ac60fb15d82515d9940f9f42d9135d3
Finn Mao <loverainys@gmail.com>
2025-01-27 17:48:31 +0800

    feat: add hover effect to move svg g element on hover

--- src/components/Map/Taiwan/index.tsx
+++ src/components/Map/Taiwan/index.tsx
@@ -1,21 +1,54 @@
-import { dotDataset, taiwanDataset } from "./data";
+import * as d3 from "d3";
+import { useRef, useState } from "react";
+import { taiwanDataset } from "./data";
 
 const Taiwan = () => {
+  const [hoveredId, setHoveredId] = useState("");
+  const svgRef = useRef(null);
+
+  const handleMouseOver = (id: string) => {
+    setHoveredId(id);
+    bringToFront(id);
+  };
+
+  const handleMouseOut = () => {
+    bringToFront("_嘉義市");
+  };
+
+  const bringToFront = (id: string) => {
+    const svg = d3.select(svgRef.current);
+    const target = svg.select(`#${id}`);
+    if (!target.empty()) {
+      target.raise();
+    }
+  };
+
   return (
     <div className="map-container">
       <svg
-        id="_全台地圖_圓點"
-        data-name="全台地圖+圓點"
+        ref={svgRef}
+        data-name="dotted-map"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 688.43 880.08"
       >
         {taiwanDataset.map((area) => {
-          const { id, name, style, d } = area;
-          return <path key={id} name={name} d={d} className={style} />;
-        })}
-        {dotDataset.map((area) => {
-          const { id, style, cx, cy, r } = area;
-          return <circle key={id} className={style} cx={cx} cy={cy} r={r} />;
+          const { id, name, d, dot } = area;
+          const { cx, cy, r, style } = dot;
+
+          return (
+            <g
+              key={id}
+              id={id}
+              onMouseOver={() => {
+                handleMouseOver(id);
+              }}
+              onMouseOut={handleMouseOut}
+              className="float"
+            >
+              <path name={name} d={d} />
+              <circle className={`${style} dot`} cx={cx} cy={cy} r={r} />
+            </g>
+          );
         })}
       </svg>
     </div>
