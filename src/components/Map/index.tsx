d3022e08348e20bcdc6bd0182fddcb3847145a25
Finn Mao <loverainys@gmail.com>
2025-02-11 17:31:27 +0800

    feat: implement year selection functionality with selectedYear and setSelectedYear

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,25 +1,60 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelection } from "../../context/SelectionContext";
 import Taiwan from "./Taiwan";
 
 const Map = () => {
   const YearSelector = () => {
     const { selectedYear, setSelectedYear } = useSelection();
+    const [showList, setShowList] = useState(false);
+
     const years = [2024, 2020, 2016, 2012, 2008];
 
+    const listRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+        if (
+          listRef.current &&
+          !listRef.current.contains(event.target as Node)
+        ) {
+          setShowList(false);
+        }
+      };
+
+      document.addEventListener("click", handleClickOutside);
+      return () => {
+        document.removeEventListener("click", handleClickOutside);
+      };
+    }, []);
+
+    const handleShowList = (event: React.MouseEvent) => {
+      event.stopPropagation();
+      setShowList((prev) => !prev);
+    };
+
+    const handleSelectYear = (year: number) => {
+      setSelectedYear(year);
+      setShowList(false);
+    };
+
     return (
-      <div className="year-selector">
-        <ul>
-          {years.map((year) => (
-            <li
-              key={year}
-              className="year-selector__item ch-text"
-              onClick={() => setSelectedYear(year)}
-            >
-              {year} 年
-            </li>
-          ))}
-        </ul>
-        <div className="year-selector__item__selected">{selectedYear} 年</div>
+      <div className="year-selector" ref={listRef}>
+        {showList && (
+          <ul>
+            {years.map((year) => (
+              <li
+                key={year}
+                className="year-selector__item ch-text"
+                onClick={() => handleSelectYear(year)}
+              >
+                {year} 年
+              </li>
+            ))}
+          </ul>
+        )}
+        <div className="year-selector__item__selected" onClick={handleShowList}>
+          {selectedYear} 年
+        </div>
       </div>
     );
   };
