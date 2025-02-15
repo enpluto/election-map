85baad147c7d7593c328d5dd5c8aea394d2e0da4
Finn Mao <loverainys@gmail.com>
2025-02-15 15:39:51 +0800

    feat: add yearlyData to SelectionContext

--- src/context/SelectionContext.tsx
+++ src/context/SelectionContext.tsx
@@ -3,7 +3,7 @@ import { Dataset2012 } from "../data/2012";
 import { Dataset2016 } from "../data/2016";
 import { Dataset2020 } from "../data/2020";
 import { Dataset2024 } from "../data/2024";
-import { FilteredDataType } from "../types/types";
+import { ElectionDataType, FilteredDataType } from "../types/types";
 
 interface SelectionContextType {
   selectedArea: string;
@@ -12,6 +12,7 @@ interface SelectionContextType {
   selectedYear: number;
   setSelectedYear: (year: number) => void;
   filteredData: FilteredDataType;
+  yearlyData: ElectionDataType;
 }
 
 interface SelectionProviderProps {
@@ -25,31 +26,31 @@ const SelectionContext = createContext<SelectionContextType | undefined>(
 export const SelectionProvider = ({ children }: SelectionProviderProps) => {
   const [selectedArea, setSelectedArea] = useState("");
   const [selectedYear, setSelectedYear] = useState(2024);
+  const [yearlyData, setYearlyData] = useState(Dataset2024);
 
   const filteredData = useMemo(() => {
-    let electionYear;
-
     switch (selectedYear) {
       case 2012:
-        electionYear = Dataset2012;
+        setYearlyData(Dataset2012);
         break;
       case 2016:
-        electionYear = Dataset2016;
+        setYearlyData(Dataset2016);
         break;
       case 2020:
-        electionYear = Dataset2020;
+        setYearlyData(Dataset2020);
         break;
       default:
-        electionYear = Dataset2024;
+        setYearlyData(Dataset2024);
         break;
     }
+
     switch (selectedArea) {
       case "":
-        return electionYear["全國"];
+        return yearlyData["全國"];
       default:
-        return electionYear[selectedArea];
+        return yearlyData[selectedArea];
     }
-  }, [selectedYear, selectedArea]);
+  }, [selectedYear, selectedArea, yearlyData]);
 
   const selectArea = (area: string) => {
     setSelectedArea(area);
@@ -68,6 +69,7 @@ export const SelectionProvider = ({ children }: SelectionProviderProps) => {
         selectedYear,
         setSelectedYear,
         filteredData,
+        yearlyData,
       }}
     >
       {children}
