ab4e1beadc6f570444af15e5b438b04eb3203fa4
Finn Mao <loverainys@gmail.com>
2025-02-15 17:01:12 +0800

    feat: implement toggle functionality for the menu

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,20 +1,47 @@
+import { useEffect, useRef, useState } from "react";
 import logoSvg from "../../assets/logo.svg";
 import menuSvg from "../../assets/menu.svg";
 import { arrowSvg, closeSvg, linkDataset } from "./data";
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const listRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (listRef.current && !listRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleShowMenu = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsOpen((prev) => !prev);
+  };
+
   return (
     <>
       <nav>
         <img src={logoSvg} alt="logo" />
-        <img src={menuSvg} alt="menu" style={{ cursor: "pointer" }} />
+        <img
+          src={menuSvg}
+          alt="menu"
+          style={{ cursor: "pointer" }}
+          onClick={handleShowMenu}
+        />
       </nav>
-      <div className="menu-wrapper">
-        <div>{closeSvg}</div>
+      <div className={`menu-wrapper ${isOpen ? "open" : ""}`} ref={listRef}>
+        <div onClick={handleShowMenu}>{closeSvg}</div>
         <ul className="menu">
           {linkDataset.map((link) => (
             <li key={link.title} className="menu__item">
-              <a href={link.url} target="_blank">
+              <a href={link.url} target="_blank" onClick={handleShowMenu}>
                 {link.title}
               </a>
               <div className="blackArrow">{arrowSvg}</div>
